b7ee2353d4f002cbad99e083e10bb5a3de765327
K <dkh1001@outlook.kr>
2021-12-01 09:44:18 +0900

    6

--- src/App.js
+++ src/App.js
@@ -1,32 +1,15 @@
 import React from "react";
 
 const employees = ["Lee", "John", "Jane"];
+const e = ["x", "y", "z"];
+const r = [...employees, ...e];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>{employees[0]}</h1>
-        <h1>{employees[1]}</h1>
-        <h1>{employees[2]}</h1>
-
-        <hr></hr>
-
-        {employees.forEach((e) => {
-          return <h1>{e}</h1>;
-          //forEach는 map과 달리 결과를 반환하지 않음
-        })}
-
-        <hr />
-
-        {employees.map((e) => {
-          return <h1>{e}</h1>;
-        })}
-
-        <hr />
-
-        {employees.map((e) => (
-          <h1>{e}</h1>
+        {r.map((a) => (
+          <h1>{a}</h1>
         ))}
       </header>
     </div>
